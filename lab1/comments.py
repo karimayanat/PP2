26bfe80378dec396b0dd4c37dc07f6ecdd35bd21
karimayanat <karimayanat07@gmail.com>
2026-01-24 21:12:40 +0500

    first commit

---
+++ lab1/comments.py
@@ -0,0 +1,13 @@
+#this is a comment
+print("Home work") #this is a comment
+#print("Hello")
+
+# this is a comment
+# written in more
+# than one line
+
+"""
+this is a comment
+written in more
+than one line
+"""
