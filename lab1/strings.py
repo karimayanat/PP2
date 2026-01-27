66be4e944c6853ee1af975317f5890c25fb8dbce
karimayanat <karimayanat07@gmail.com>
2026-01-27 17:05:49 +0500

    first commit

---
+++ lab1/strings.py
@@ -0,0 +1,7 @@
+print("Hello, KBTU!")
+print("It's fine")
+print("The book title is 'Hobbit'")
+a = """Strings in python are surrounded
+by either single quotation marks,
+or double quotation marks."""
+print(a)
