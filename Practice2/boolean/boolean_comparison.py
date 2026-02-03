a144f7787b4a27541628481afe61b28ed81102ca
karimayanat <karimayanat07@gmail.com>
2026-02-03 16:36:22 +0500

    Add Practice2 - Python control flow examples (boolean)

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,10 @@
+print(50 > 8)
+print(50 == 8)
+print(50 < 8)
+
+a = 150
+b = 9
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,6 @@
+x = 6
+print(x > 0 and x < 100)
+
+print(x > 7 or x < 6)
+
+print(not(x > 3 and x < 10))
